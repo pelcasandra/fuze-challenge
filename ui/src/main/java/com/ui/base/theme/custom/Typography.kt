669e711433947199fff67815aa5967ca11309fd4
Jvrni <joao.varani@gmail.com>
2023-08-07 16:33:36 -0300

    Created components and some files changed

--- ui/src/main/java/com/ui/base/theme/custom/Typography.kt
+++ ui/src/main/java/com/ui/base/theme/custom/Typography.kt
@@ -5,25 +5,24 @@ import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
-import com.ui.base.theme.custom.Typography.Font.Montserrat
+import com.ui.base.theme.custom.Typography.Font.Roboto
 import com.fuze.R
 
 sealed class Typography {
     object Font {
-        val Montserrat = FontFamily(
-            Font(R.font.montserrat_light, FontWeight.W300),
-            Font(R.font.montserrat_regular, FontWeight.W400),
-            Font(R.font.montserrat_medium, FontWeight.W500),
-            Font(R.font.montserrat_semibold, FontWeight.W600),
-            Font(R.font.montserrat_bold, FontWeight.W700)
+        val Roboto = FontFamily(
+            Font(R.font.roboto_light, FontWeight.W300),
+            Font(R.font.roboto_regular, FontWeight.W400),
+            Font(R.font.roboto_medium, FontWeight.W500),
+            Font(R.font.roboto_bold, FontWeight.W700)
         )
     }
 
     object Label {
-        val large = TextStyle(fontSize = 21.sp, lineHeight = 24.sp, fontFamily = Montserrat, fontWeight = FontWeight.W600)
-        val medium = TextStyle(fontSize = 18.sp, lineHeight = 24.sp, fontFamily = Montserrat, fontWeight = FontWeight.W500)
-        val small = TextStyle(fontSize = 16.sp, lineHeight = 20.sp, fontFamily = Montserrat, fontWeight = FontWeight.W600)
-        val xsmall = TextStyle(fontSize = 13.sp, lineHeight = 16.sp, fontFamily = Montserrat, fontWeight = FontWeight.W500)
-        val xxsmall = TextStyle(fontSize = 11.sp, lineHeight = 16.sp, fontFamily = Montserrat, fontWeight = FontWeight.W400)
+        val large = TextStyle(fontSize = 21.sp, lineHeight = 24.sp, fontFamily = Roboto, fontWeight = FontWeight.W700)
+        val medium = TextStyle(fontSize = 18.sp, lineHeight = 24.sp, fontFamily = Roboto, fontWeight = FontWeight.W600)
+        val small = TextStyle(fontSize = 16.sp, lineHeight = 20.sp, fontFamily = Roboto, fontWeight = FontWeight.W500)
+        val xsmall = TextStyle(fontSize = 13.sp, lineHeight = 16.sp, fontFamily = Roboto, fontWeight = FontWeight.W400)
+        val xxsmall = TextStyle(fontSize = 11.sp, lineHeight = 16.sp, fontFamily = Roboto, fontWeight = FontWeight.W300)
     }
 }
